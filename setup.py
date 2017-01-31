3df5cd74698e2f0de2b67ae946adbea160a8ea8b
Mike Lang <mikelang3000@gmail.com>
2017-01-31 05:19:17 -0800

    Basic skeleton

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='pygbemu',
+	version='0.0.1',
+	author='Mike Lang',
+	author_email='mikelang3000@gmail.com',
+	description='A Game Boy emulator',
+	packages=find_packages(),
+	install_requires=[
+	],
+)
